070db48155d88ed4a29c2f2282e22ece85d6752c
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-04-11 15:39:34 +0200

    feat: add milestone1 : retrive the list of actors from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [actor, setActor] = useState([])
 
+  function fetchActor() {
+    axios.get("https://www.freetestapi.com/api/v1/actors")
+      .then((res) => setActor(res.data))
+  }
+
+  useEffect(fetchActor, [])
+  console.log(actor)
   return (
     <>
-
+      <div>
+        <h1>Liasta Attori</h1>
+      </div>
+      <hr />
+      <ul>
+        {actor.map((element) => <li key={element.id}>{element.name}</li>)}
+      </ul>
     </>
   )
+
 }
 
 export default App
